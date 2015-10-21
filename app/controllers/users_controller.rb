3842a61768dc9e4c98095de3ece97a61391d6711
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-10-21 14:48:05 -0700

    added backend for User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      #flash[:success] = "Welcome to PINterview!"
+      redirect_to :root
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def destory
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                 :password_confirmation, :grad_class, :major)
   end
 end
