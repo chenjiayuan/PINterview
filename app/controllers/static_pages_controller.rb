f6fdd078736f922e8064952166a0bc01537804ea
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-11-04 08:57:02 -0800

    loginlogout finish

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+	before_action :require_user, only: [:home]
+
 	def home
 		@pins = Pin.all
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   match 'static_pages#home',  to: 'static_pages#home', via: 'get'
   resources :pins
 
-  get 'signup'  => 'users#new' 
+  get 'signup'  => 'users#new'
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
