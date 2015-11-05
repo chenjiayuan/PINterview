62406e9fc5cd50d8377d32dd1e581be78282ab97
kieykouch <ki.ey.kouch@gmail.com>
2015-11-04 21:54:45 -0800

    fixed create new pin form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -16,11 +16,7 @@ class PinsController < ApplicationController
   	end 
 
     def show
-      if current_user.id == params[:id]
-        @pin = Pin.find(params[:id])
-      else
-        @pin = nil
-      end
+      @pin = Pin.find(params[:id])      
     end
 
     def edit
