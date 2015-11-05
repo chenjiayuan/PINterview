32ccd6055deef5f7af683984dde9db3ffcb67187
kieykouch <ki.ey.kouch@gmail.com>
2015-11-04 18:15:30 -0800

    Change Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do  
   root :to => 'static_pages#home' 
+  
   get 'pins/new', to:'pins#new'
-  #post 'pins' => 'pins#create'
+  
   match 'static_pages#home',  to: 'static_pages#home', via: 'get'
-  resources :pins
+  
 
   get 'signup'  => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
   resources :users
+  resources :pins
 end
