47e2301f83b0ed700ea8fd4bb89c5e544918cfc5
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-11-06 20:37:10 -0800

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 a1 = Pin.create(position: "Software", company: "Yahoo", date: "10/20/2015",
 	difficulty: "Easy", type_interview: "Technical", attire: "Yes", 
 	questions: "How to make string double its size",like_count: 0, 
-	length: "30 Minutes", description: "I found the bug!",created_at: Time.now, updated_at: Time.now)
+	length: "30 Minutes", description: "I found the bug!")
 
 #a2 = User.create(username: "Kiey", password: "John", email: "ki.ey.kouch@berkeley.edu",
 #	grad_class: "2016", major: "Computer Science")
