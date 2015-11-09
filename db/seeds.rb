4ed9d081d7bff6adc687d0d72c21d9fc16059c67
kieykouch <ki.ey.kouch@gmail.com>
2015-11-08 19:59:19 -0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-a1 = Pin.create(position: "Software", company: "Yahoo", date: "10/20/2015",
+100.times{ |i| 
+Pin.create(position: "Software", company: "Yahoo", date: "10/20/2015",
 	difficulty: "Easy", type_interview: "Technical", attire: "Yes", 
 	questions: "How to make string double its size",like_count: 0, 
-	length: "30 Minutes", description: "I found the bug!")
+	length: "30 Minutes", description: "I found the bug!")}
 
 #a2 = User.create(username: "Kiey", password: "John", email: "ki.ey.kouch@berkeley.edu",
 #	grad_class: "2016", major: "Computer Science")
