84bac79b593823a24a0a3e03fc64219d111c7c3c
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-11-24 10:55:02 -0800

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-100.times{ |i| 
-Pin.create(position: "Software", company: "Yahoo", date: "10/20/2015",
-	difficulty: "Easy", type_interview: "Technical", attire: "Yes", 
-	questions: "How to make string double its size",like_count: 0, 
-	length: "30 Minutes", description: "I found the bug!")}
+User.create(username: "CJYseed", password: "111111", email: "1@berkeley.edu",
+  grad_class: "2016", major: "EECS")
 
-#a2 = User.create(username: "Kiey", password: "John", email: "ki.ey.kouch@berkeley.edu",
-#	grad_class: "2016", major: "Computer Science")
+
+companies = ['Google','Yahoo','Uber','Oracle','Amazon',
+  'Airbnb','Twitter','Microsoft','HP','IBM','Costco','Github',
+  'Pivotal','Linkedin','Apple','Quora','Pinterest']
+
+positions = ['CEO','CFO','COO','Software Developer','Tester','Manager','Mechanician']
+
+100.times { |i|
+Pin.create(position: positions.sample, company: companies.sample, date: "11-24-2015",
+	difficulty: "Easy", type_interview: "Technical", attire: "Onsite", 
+	questions: "How to make string double its size",like_count: 0, 
+	length: "30 Minutes", description: "I found the bug!", user_id: 1, position_type: "full-time")
+}
