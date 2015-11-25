e5f82617954cb37959c4993dc020a2aa19b3c5b5
Jiayuan Chen <chenjiayuancz@gmail.com>
2015-11-24 20:46:38 -0800

    try one more

---
+++ public/assets/datepicker_ki-9284d95c6b8864131feb68c810e22efda688b32cfb4d65409e1c6a33186931d5.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".datepicker").pickadate({
+    selectYears: true,
+    selectMonths: true,
+    max: true
+  });
+});
---
+++ public/assets/search-9e64a1e29fcf597f24f37381889e1555889c57720836e611d8a0c23cbfb9a7fc.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".advanced-search-wrapper").hide();
+  $(".Advanced-Search").click(function(){
+    $(".advanced-search-wrapper").toggle(400); 
+  });
+});
